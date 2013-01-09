2615198c07a4379afb6f36b1580af84a9c1fbe81
ikeaboy <a.hiolle@herts.ac.uk>
2013-01-09 19:31:31 +0000

    Working Sound file wav only loader That is it, rest is coming soon

---
+++ devBranch/main/mainSimul.cpp
@@ -0,0 +1,27 @@
+/*
+ * mainSimul.cpp
+ *
+ * Main file where you start the simulation
+ *
+ *  Created on: 9 Jan 2013
+ *      Author: antoine
+ */
+#include <soundInOut.h>
+
+int main (){
+
+
+/**
+ * Just new SoundWrapper does nothing
+ */
+SoundWrapper * soundWrap=new SoundWrapper();
+
+	soundWrap->setFile("../soundsSamples/bip.wav");
+
+
+
+
+}
+
+
+
